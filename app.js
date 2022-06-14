56d3ffeda3e392bf10d2e72b46b8091c2e9c733d
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-06-15 02:10:54 +0500

    second commit

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-
-// Take input from user==================================
+ // Take input from user==================================
 
 var input = prompt("Enter a word")
 
 
 // Declare letters=========================================
+ var alphabets=["a","b","c","d","e","f","i","j","k","k","l","m","n","o","p"
+  ,"q","r","s","t","u","v","w","x","y","z"];
 
-var alphabets=["a","b","c","d","e","f","i","j","k","k","l","m","n","o","p"
-,"q","r","s","t","u","v","w","x","y","z"];
+// Create empty arrray======================================================
 
-var random;
-random=Math.floor(Math.random()*100);
-console.log(random)
+var lettersArray=[];
+var charactersLength=alphabets.length;
 
-function randomWords() {
-    var guess=input.value
-    if(guess === random){
-        document.write="You got right"
-    }
-    
-}
-
+var random = alphabets[Math.floor(Math.random()*alphabets.length)];
+console.log(random);
