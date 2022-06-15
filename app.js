85f8ed4e5bffab88e1ef6a1ea9f5690a9c706a8c
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-06-15 16:02:17 +0500

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
  // Take input from user==================================
 
-var input = prompt("Enter a word")
+ var input = prompt("Enter a word which consist not more than 4 characters");
+
+ var guess=input.value;
 
 
 // Declare letters=========================================
@@ -10,7 +12,41 @@ var input = prompt("Enter a word")
 // Create empty arrray======================================================
 
 var lettersArray=[];
-var charactersLength=alphabets.length;
 
-var random = alphabets[Math.floor(Math.random()*alphabets.length)];
-console.log(random);
+// Create function on button==============================================================
+function randomWords() {
+
+
+// Declare random numbers length to 4========================================================
+  for(var i=1; i<= Math.floor(Math.random())+4; i++) {
+    
+// Replacing random numbers to letters which is in lettersArray===============================
+    random = alphabets [Math.floor(Math.random()*alphabets.length)];
+
+// Push randon characters to declared array==============================================
+    lettersArray.push(random);
+}
+
+
+// Join  lettersArrays elements which is random letters into string============================
+lettersArray=lettersArray.join("");
+
+// Checking user guess against random generated string==========================================
+if (guess === lettersArray) {
+  alert("You got it right");
+
+}
+
+else if (guess !== lettersArray){
+  alert("Wrong");
+}
+
+
+// Print random generated string===============================================================
+console.log(lettersArray);
+
+}
+
+// End of function===================================================================
+
+
